1ad4345ccfc50553540aa8b77c51b0c00a6881c3
superjke <superjke@noemail.com>
2019-12-21 15:08:27 +0000

    Removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Personal Expenses',
       theme: ThemeData(
           primarySwatch: Colors.green,
